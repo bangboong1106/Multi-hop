a07c0736b2298ae5c0ec8d2e00662a725fbf7c91
bangboong1106 <nguyenbangsmile1999@gmail.com>
2020-05-20 13:32:54 +0700

    aa

---
+++ MultiShop/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultiShop.Models;
+namespace MultiShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        MultiShopDbContext db = new MultiShopDbContext();
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MultiShop/Controllers/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultiShop.Models;
+namespace MultiShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        MultiShopDbContext db = new MultiShopDbContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Category(int CategoryID = 0)
+        {
+            if (CategoryID != 0)
+            {
+    
+                var model = db.Products.Where(p => p.CategoryId == CategoryID);
+                return View(model);
+            }
+
+            return View();
+        }
+        public ActionResult Detail(int id)
+        { 
+            var model = db.Products.Find(id);
+
+            // Tăng số lần xem
+            model.Views++;
+            db.SaveChanges();
+
+            // Lấy cookie cũ tên views
+            var views = Request.Cookies["views"];
+            // Nếu chưa có cookie cũ -> tạo mới
+            if (views == null)
+            {
+                views = new HttpCookie("views");
+            }
+            // Bổ sung mặt hàng đã xem vào cookie
+            views.Values[id.ToString()] = id.ToString();
+            // Đặt thời hạn tồn tại của cookie
+            views.Expires = DateTime.Now.AddMonths(1);
+            // Gửi cookie về client để lưu lại
+            Response.Cookies.Add(views);
+
+            // Lấy List<int> chứa mã hàng đã xem từ cookie
+            var keys = views.Values
+                .AllKeys.Select(k => int.Parse(k)).ToList();
+            // Truy vấn háng đãn xem
+            ViewBag.Views = db.Products
+                .Where(p => keys.Contains(p.Id));
+            //top 10 san pham
+            ViewBag.Top = db.Products.Where(p => p.Id > 0).OrderByDescending(p => p.Views).Take(5).ToList();
+            return View(model);
+        }
+        public ActionResult Search(String SupplierId = "", int CategoryId = 0, String Keywords = "")
+        {
+            if (SupplierId != "")
+            {
+                var model = db.Products
+                    .Where(p => p.SupplierId == SupplierId);
+                return View(model);
+            }
+            else if (CategoryId != 0)
+            {
+                var model = db.Products
+                    .Where(p => p.CategoryId == CategoryId);
+                return View(model);
+            }
+            else if (Keywords != "")
+            {
+                var model = db.Products
+                    .Where(p => p.Name.Contains(Keywords));
+                return View(model);
+            }
+            return View(db.Products);
+        }
+
+    }
+}
